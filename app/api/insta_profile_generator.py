ae4a691ff2ec380bec2bd89cfab1970d8a003170
Renato Ramos <ramossotorenato@gmail.com>
2024-08-13 21:58:27 -0500

    post to get

--- app/api/insta_profile_generator.py
+++ app/api/insta_profile_generator.py
@@ -1,5 +1,5 @@
 # app/api/insta_profile_generator.py
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Query
 from app.utils.selenium_driver import configure_driver
 from app.utils.search_tools import (
     calculate_dates, 
@@ -9,27 +9,27 @@ from app.utils.search_tools import (
     extract_links,
     save_json
 )
-from app.schemas import SearchRequest, SearchResult  # Import the data models
+from app.schemas import SearchResult  # Import the data models
 
 router = APIRouter()
 
-@router.post("/search/", response_model=SearchResult)
-def google_search_with_tools(request: SearchRequest):
+@router.get("/search/", response_model=SearchResult)
+def google_search_with_tools(search_term: str = Query(...), date_option: str = Query(...)):
     try:
         driver = configure_driver()
-        search_on_google(driver, request.search_term)
-        apply_filters(driver, request.date_option)
+        search_on_google(driver, search_term)
+        apply_filters(driver, date_option)
         result_count = extract_result_count(driver)
         hrefs = extract_links(driver)
         
-        start_date, end_date = calculate_dates(request.date_option)
+        start_date, end_date = calculate_dates(date_option)
         results_json = {
             "result_count": result_count,
             "date_range": f"{start_date} - {end_date}",
             "results": hrefs
         }
         
-        filename = save_json(results_json, request.search_term, start_date, end_date)
+        filename = save_json(results_json, search_term, start_date, end_date)
         
         driver.quit()
         
