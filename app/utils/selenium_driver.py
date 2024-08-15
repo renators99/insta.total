e495b3b18145fa71602e2d6a2a1f9e87121d33d5
Renato Ramos <ramossotorenato@gmail.com>
2024-08-14 21:21:13 -0500

    meta hashtags modified

--- app/utils/selenium_driver.py
+++ app/utils/selenium_driver.py
@@ -13,6 +13,6 @@ def init_driver():
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--window-size=1920,1080")
-    #chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     driver = uc.Chrome(options=chrome_options, use_subprocess=False)
     return driver
